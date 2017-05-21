ff21dd31a56d4fb36565d9329f1c217c83de1b38
Amanda Dearman <amandajoanski@gmail.com>
2017-05-21 13:24:08 -0700

    added game logic from unit 9 and 10

--- js/main.js
+++ js/main.js
@@ -1,17 +1,52 @@
  console.log("Up and running!");
 
- var cards = ["queen", "queen", "king", "king"]
- var cardsInPlay = []
- var cardOne = cards[0];
- cardsInPlay.push('cardOne');
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped king");
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[2]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
-}
+ var cards = [
+ {
+ 	rank: 'queen',
+ 	suit: 'hearts',
+ 	cardImage: "images/queen-of-hearts.png"
+ },
+ {	rank: 'queen',
+ 	suit: 'diamonds',
+ 	cardImage: "images/queen-of-diamonds.png"
+ },
+ {	rank: 'king',
+ 	suit: 'hearts',
+ 	cardImage: "images/king-of-hearts.png"
+ },
+ {
+ 	rank: 'king',
+ 	suit: 'diamonds',
+ 	cardImage: "images/king-of-diamonds.png"
+ }
+ ];
+ var cardsInPlay = [];
+
+var checkForMatch = function () {
+ 	 if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+  } else {
+      console.log("Sorry, try again.");
+  }
+ };
+ var flipCard = function (cardId){
+ 	console.log("User flipped" + " " +cards[cardId].rank);
+ 	cardsInPlay.push(cards[cardId].rank);
+ 	console.log(cards[cardId].cardImage);
+ 	console.log(cards[cardId].suit);
+ 	checkForMatch();
+ 	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[2]) {
+ 	alert("You found a match!");
+ 	} else {
+ 	alert("Sorry, try again.");
+ }
+ };
+
+ flipCard(0);
+ flipCard(2);
+
+ 
+	
+
 
  
